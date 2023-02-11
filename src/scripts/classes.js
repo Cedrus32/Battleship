899be594fc366a5f2fc05746ab100384413fb15a
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-02-11 07:43:23 -0600

    Create gameboard class

--- src/scripts/classes.js
+++ src/scripts/classes.js
@@ -1,9 +1,9 @@
 class Ship {
-    constructor(length = null) {
-        this.placed = false;
+    constructor(length = null, coords = null) {
         this.length = length;
         this.hits = 0;
         this.sunk = false;
+        this.coords = coords;
     }
 
     isPlaced() {
@@ -11,6 +11,7 @@ class Ship {
     }
     logHit() {
         this.hits += 1;
+        this.isSunk();
     }
     isSunk() {
         if (this.hits === this.length) {
@@ -18,18 +19,115 @@ class Ship {
         }
     }
 }
-
 class Gameboard {
     constructor() {
+        this.grid = [[[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []],
+                     [[], [], [], [], [], [], [], [], [], []]];
+        this.ships = [];
+        this.shipsSunk = 0;
+    }
 
+    placeShip(startCoord, dir, shipLen) {
+        let coordSet = this.getCoords(startCoord, dir, shipLen);
+        if (this.setIsValid(coordSet)) {
+            this.ships.push(makeShip(shipLen, coordSet));
+        }
+        
+    }
+    getCoords(startCoord, dir, shipLen) {
+        let coordSet = [startCoord];
+        let i = 0;
+        if (dir === 'h') {
+            let xPosition = parseInt(startCoord.split('')[0]);
+            while (i < shipLen - 1) {
+                xPosition += 1;
+                xPosition.toString();
+                let newCoord = xPosition + startCoord.split('')[1];
+                coordSet.push(newCoord);
+                i++;
+            }
+        } else if (dir === 'v') {
+            let yPosition = parseInt(startCoord.split('')[1]);
+            while (i < shipLen - 1) {
+                yPosition += 1;
+                yPosition.toString();
+                let newCoord = startCoord.split('')[0] + yPosition;
+                coordSet.push(newCoord);
+                i++;
+            }
+        }
+        return coordSet;
+    }
+    setIsValid(coordSet) {
+        for (let i = 0; i < coordSet.length; i++) {
+            let newX = parseInt(coordSet[i].split('')[0]);
+            let newY = parseInt(coordSet[i].split('')[1]);
+            for (let j = 0; j < this.ships.length; j++) {
+                let ship = this.ships[j];
+                for (let k = 0; k < ship.coords.length; k++) {
+                    let placedX = parseInt(ship.coords[k].split('')[0]);
+                    let placedY = parseInt(ship.coords[k].split('')[1]);
+                    let buffer = [placedX - 1, placedX + 1, placedY - 1, placedY + 1];
+                    if (newX >= buffer[0] && newX <= buffer[1] && newY >= buffer[2] && newY <= buffer[3]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    receiveAttack(coord) {
+        let outcome = this.isHit(coord);
+        let hit = outcome[0];
+        let ship = outcome[1];
+        if (hit) {
+            this.markBoard(coord, 'x');
+            ship.logHit();
+            if (ship.sunk) {
+                this.shipsSunk += 1;
+            }
+        } else if (!hit) {
+            this.markBoard(coord, 'o');
+        }
+    }
+    isHit(attackCoord) {
+        for (let i = 0; i < this.ships.length; i++) {
+            let ship = this.ships[i];
+            for (let j = 0; j < ship.coords.length; j++) {
+                let shipCoord = ship.coords[j];
+                if (attackCoord === shipCoord) {
+                    return [true, ship];
+                }
+            }
+        }
+        return [false, undefined];
+    }
+    markBoard(coord, mark) {
+        let x = parseInt(coord.split('')[0]);
+        let y = parseInt(coord.split('')[1]);
+        this.grid[y][x] = mark;
+    }
+
+    isLoser() {
+        if (this.shipsSunk === this.ships.length) {
+            return true;
+        }
     }
 }
 
-function makeShip(length) {
-    let ship = new Ship(length);
+function makeShip(length, coords) {
+    let ship = new Ship(length, coords);
     return ship;
 }
-
 function makeGameboard() {
     let gameboard = new Gameboard();
     return gameboard;
--- tests/classes.test.js
+++ tests/classes.test.js
@@ -1,40 +1,97 @@
 import { default as makeShip, makeGameboard } from '../src/scripts/classes.js';
 
 let ship;
-test.skip('create empty object', () => {
-    ship = makeShip();
-    expect(ship.placed).toBeFalsy();
-    expect(ship.length).toBeNull();
-    expect(ship.hits).toEqual(0);
-    expect(ship.sunk).toBeFalsy();
-})
-test.skip('create ship with length', () => {
+test('create ship with length', () => {
     ship = makeShip(5);
     expect(ship.length).toEqual(5);
 })
-test.skip('change placed bool', () => {
-    ship.isPlaced();
-    expect(ship.placed).toBeTruthy();
+test('create ship with coords', () => {
+    ship = makeShip(5, ['00', '01', '02', '03', '04']);
+    expect(ship.coords).toEqual(['00', '01', '02', '03', '04']);
 })
-test.skip('change hit count', () => {
+test('change hit count', () => {
     ship.logHit();
     expect(ship.hits).toEqual(1);
 })
-test.skip('change sunk book', () => {
+test('change sunk bool', () => {
     let i = 0;
     while (i < 4) {
         ship.logHit()
         i++;
     }
-    ship.isSunk();
     expect(ship.sunk).toBeTruthy();
 })
 
-let gameboard;
-test('create empty gameboard', () => {
-    gameboard = makeGameboard();
-    expect(gameboard).toEqual({});
+
+let gameboard = makeGameboard();
+// HORIZONTAL PLACEMENT
+test('creates horizontal coordinate set', () => {
+    expect(gameboard.getCoords('00', 'h', 5)).toEqual(['00', '10', '20', '30', '40']);
 })
-test('', () => {
-    expect().toEqual();
+test('gameboard returns true for valid horizontal coordSet', () => {
+    expect(gameboard.setIsValid(['00', '10', '20', '30', '40'])).toBeTruthy();
 })
+test('places ship at valid horizontal coords', () => {
+    gameboard.placeShip('00', 'h', 5);
+    expect(gameboard.ships[0] !== undefined).toBeTruthy();
+})
+// VERTICAL PLACEMENT
+test('creates vertical coordinate set', () => {
+    expect(gameboard.getCoords('02', 'v', 5)).toEqual(['02', '03', '04', '05', '06']);
+})
+test('gameboard returns true for valid vertical coordSet', () => {
+    expect(gameboard.setIsValid(['02', '03', '04', '05', '06'])).toBeTruthy();
+})
+test('places ship at valid vertical coords', () => {
+    gameboard.placeShip('02', 'v', 5);
+    expect(gameboard.ships[1] !== undefined).toBeTruthy();
+})
+test('gameboard returns false for invalid coordSet', () => {
+    expect(gameboard.setIsValid(['00', '01', '02', '03', '04'])).toBeFalsy();
+})
+// HITS
+test('gameboard returns true if hit', () => {
+    expect(gameboard.isHit('00')[0]).toBeTruthy();
+})
+test('gameboard marks hit', () => {
+    gameboard.markBoard('00', 'x');
+    expect(gameboard.grid[0][0]).toEqual('x');
+})
+// MISSES
+test('gameboard returns false if miss', () => {
+    expect(gameboard.isHit('33')[0]).toBeFalsy();
+})
+test('gameboard marks miss', () => {
+    gameboard.markBoard('33', 'o');
+    expect(gameboard.grid[3][3]).toEqual('o');
+})
+
+// TRACK SUNK SHIPS
+test('gameboard tracks sunk ships', () => {
+    let hitList = ['02', '03', '04', '05', '06'];
+    for (let i = 0; i < hitList.length; i++) {
+        gameboard.receiveAttack(hitList[i]);
+    }
+    console.log(gameboard.ships[1]);
+    expect(gameboard.shipsSunk).toEqual(1);
+})
+
+// TRACK WIN/LOSE
+test('returns false when all ships are not sunk', () => {
+    expect(gameboard.isLoser()).toBeFalsy();
+})
+test('returns true when all ships are sunk', () => {
+    let hitList = ['00', '10', '20', '30', '40'];
+    for (let i = 0; i < hitList.length; i++) {
+        gameboard.receiveAttack(hitList[i]);
+    }
+    console.log(gameboard.ships[0]);
+    expect(gameboard.isLoser()).toBeTruthy();
+})
+
+//// board "places" ships by calling makeShip function and pushing it to ships[] array
+//// board checks received attacks agains ships[]...
+//// ... then logs hits & misses in array spread representing play grid
+//// ... and updates ship hits if needed...
+//// ...... and checks if the ship is sunk
+//// board checks how many ships are sunk (win)
